9008053f962d7a372e1d29245d47317237bcae35
Nguyen Thanh Luan <luann4099@gmail.com>
2022-04-25 13:57:47 +0700

    save image url

--- src/pages/GalleryImage.js
+++ src/pages/GalleryImage.js
@@ -1,21 +1,61 @@
-import React, {useState} from 'react';
-import {getStorage, uploadBytes, ref} from "firebase/storage";
+import React, {useEffect, useState} from 'react';
+import {getStorage, uploadBytes, ref, getDownloadURL} from "firebase/storage";
+import {db} from "../firebase";
+import {addDoc, collection, getDocs} from "firebase/firestore";
 
 const GalleryImage = () => {
     const [file, setFile] = useState(null);
+    const [files, setFiles] = useState([]);
+    const storage = getStorage();
 
     const onChangeFile = (evt) => {
         setFile(evt.target.files[0]);//lay file từ input
     };
 
     const uploadFile = async (evt) => {
-        const storage = getStorage();//base storage
-        const myRef = ref(storage, "images/image.png");
-        await uploadBytes(myRef, file, "images/image.png");
+        const storage = getStorage();//base storage //unix
+        const fileName = `images/${Date.now()}image.png`;
+        const myRef = ref(storage, fileName);//tao ref
+        try {
+            await uploadBytes(myRef, file, fileName);
+            //lưu lại file vào firestore
+            const collectionRef = collection(db, 'images');
+
+            const pathRef = ref(storage, fileName);
+            const url = await  getDownloadURL(pathRef);
+            await addDoc(collectionRef, {
+                url: url
+            });
+        } catch (e) {
+
+        }
     }
+
+    useEffect(() => {
+        (async () => {
+            const collectionRef = collection(db, 'images');
+            const collectionSnapShot = await getDocs(collectionRef);
+            const urls = [];
+            collectionSnapShot.forEach(doc => {
+                const {url} = doc.data();
+                urls.push(url);
+            });
+            setFiles(urls);
+            console.log(urls);
+        })()
+    }, []);
+
+/*    const getDownloadUrl = async (url) => {
+        const pathRef = ref(storage, url);
+        const data = await  getDownloadURL(pathRef);
+        console.log(data);
+        return data;
+    }*/
+
     return (
         <div>
             <input type="file" onChange={onChangeFile}/>
+            {files.map(url => <img key={url} src={url} alt=""/>)}
             <button onClick={uploadFile}>Upload</button>
         </div>
     );
