a572ebf78af2527fa702a9b5aaf0386625ba0d61
Nguyen Thanh Luan <luann4099@gmail.com>
2022-04-25 11:02:23 +0700

    update code

--- src/pages/todos/List.js
+++ src/pages/todos/List.js
@@ -1,26 +1,51 @@
 import * as React from 'react';
 import {useEffect} from "react";
 import {db} from "../../firebase";
-import {collection, getDocs} from 'firebase/firestore';
+import {collection, getDocs, deleteDoc, doc, onSnapshot} from 'firebase/firestore';
 import {Link} from "react-router-dom";
 
 const List = () => {
     const [todos, setTodos] = React.useState([]);
+    let unsub = null;
     useEffect(() => {
         console.log('List');
         (async () => {
             const collectionRef = collection(db, 'todos');
-            const collectionSnapShot = await getDocs(collectionRef);
-            const localTodos = [];
-            collectionSnapShot.forEach(doc => {
-                //console.log(doc.data());
-                localTodos.push(
-                    {id: doc.id,
-                    message: doc.data().message});
+
+            //lấy về snapshot 1 lần
+            //const collectionSnapShot = await getDocs(collectionRef);
+
+            //subscribe vào observable của data
+            //observable / pub-sub
+
+            unsub = onSnapshot(collectionRef, (snapShot) => {
+                const localTodos = [];
+                console.log("Có sự thay đổi dữ liệu");
+                snapShot.forEach(doc => {
+                    localTodos.push({
+                        id: doc.id,
+                        message: doc.data().message
+                    });
+                });
+                setTodos(localTodos);
             });
-            setTodos(localTodos);
+
+            /*     const localTodos = [];
+                 collectionSnapShot.forEach(doc => {
+                     localTodos.push(
+                         {
+                             id: doc.id,
+                             message: doc.data().message
+                         });
+                 });*/
+
         })();
     }, []);
+
+    const deleteNote = async (id) => {
+        const docRef = doc(db, 'todos', id);
+        await deleteDoc(docRef);
+    }
 
     return (
         <div>
@@ -28,6 +53,10 @@ const List = () => {
                 {todos.map((todo, index) => (
                     <li key={index}>{todo.message}
                         <Link to={`/edit?id=${todo.id}`}>Edit</Link>
+                        <button onClick={() => {
+                            deleteNote(todo.id)
+                        }}>Delete
+                        </button>
                     </li>
                 ))}
             </ul>
